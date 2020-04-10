c70bc35ae0e4402c78beae80a9157218a185e2c8
higorasilverio <higorasilverio@outlook.com>
2020-04-10 15:29:21 -0300

    Password check: 2002 - Final

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("*** Password check: 2002 ***");
+		System.out.println();
+		
+		boolean access = false;
+			
+		while (access == false) {
+			System.out.print("Enter the password: ");
+			int password = scanner.nextInt();
+			if (password == 2002) {
+				System.out.println("Access allowed!");
+				access = true;
+			}
+			else System.out.println("Invalid password!");
+			System.out.println();
+		}
+		
+		scanner.close();
+		
+	}
+
+}
